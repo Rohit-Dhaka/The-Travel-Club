c8a80a03b74c335d615079e329984d0b4481a51b
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2024-03-28 15:53:32 +0530

    add bg img

--- assets/js/app.js
+++ assets/js/app.js
@@ -150,6 +150,6 @@ document.addEventListener("DOMContentLoaded", function () {
       let loadingOverlay = document.getElementById("loadingOverlay");
       loadingOverlay.style.display = "none";
       document.querySelector("body").classList.remove("overflow_hidden");
-    }, 3000);
+    }, );
   });
 });
